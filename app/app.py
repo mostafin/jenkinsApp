f69e53904fe7a61f64c2a7076427407e9709129c
mostafin <ostafm@gmail.com>
2019-05-10 16:45:23 +0200

    .

--- app/app.py
+++ app/app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World V.1.0.1!'
+    return 'v1.0.0!'
 
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0',port=4000)
